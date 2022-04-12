6508a233a0f976166955a177278435152e1fba29
半田裕紀 <handayuuki@handayuukis-MacBook-Air.local>
2022-04-12 15:41:06 +0900

    chapter4

---
+++ JavaWcp2/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		
+		String str1 = "lixcel";
+		final int LUCKY_NUMBER = 7;		
+		System.out.println(str1);
+		str1 = "再代入";
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
