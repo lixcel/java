4a1424224845baeabaf4c1e3ee0304be8c35f987
lixcel <lixcelv@gmail.com>
2022-04-18 16:16:27 +0900

    chapter6

---
+++ JavaWcp2/src/Chapter06.java
@@ -0,0 +1,22 @@
+
+public class Chapter06 {
+	public static void main(String[] args) {
+		int val = 2 + 5;
+		System.out.println(val);
+		
+		//インクリメント		
+		val++;
+		System.out.println(val);
+		
+		//掛け算
+		val *= 5;
+		System.out.println(val);
+		
+		boolean bool = val == 50;
+		System.out.println(bool);
+		
+		bool = (val < 50) && (val % 10 == 0);
+		System.out.println(bool);		
+	}
+
+}
