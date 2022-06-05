6a3174398b66dd7bdef4419391c3567c35d0b39f
lixcel <lixcelv@gmail.com>
2022-06-05 10:26:47 +0900

    九九

---
+++ JavaWcp2/src/noath/sample4_17.java
@@ -0,0 +1,13 @@
+package noath;
+
+public class sample4_17 {
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 9; i++) {
+			for (int x = 1; x <=9; x++) {
+				System.out.printf(" %2d", i * x);
+			}
+			System.out.println();
+		}
+	}
+}
